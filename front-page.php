1f0c11da686a301b37acd79080c449984c5a3347
Anthony Villarroel <60784810+Villatony99@users.noreply.github.com>
2021-05-26 21:59:50 -0500

    trabajo en clase

--- front-page.php
+++ front-page.php
@@ -1,9 +1,55 @@
 <?php get_header(); ?>
 <main>
-<div class="flex items-center space-x10">
-<div class="w-1/12">
-<img
-class
-</div></div>
+<div class="flex items-center space-x-10">
+    <div class="w-1/12" >
+        <img 
+            class="rounded-full"
+            src="<?php echo get_template_directory_uri()?>/assets/img/anthony.jpg" 
+            alt="imagen de perfil anthony">
+    </div>
+<div class="w-6/12">
+    <b>Biografia</b>
+    <p>Mis nombres completos son Anthony Sebastian Villarroel Sanchez, tengo 20 años. Actualmente estoy cursando quinto semestre en la carrera de Analisis en Sistemas, en el Instituto Tecnologio "Quito"</p>
+</div>
+</div>
 
+<div class="flex items-center space-x-10">
+    <div class="w-1/12" >
+        <img 
+            class="rounded-full"
+            src="<?php echo get_template_directory_uri()?>/assets/img/balon.jpg" 
+            alt="imagen de perfil anthony">
+    </div>
+    <div class="w-6/12">
+        <b>Intereses</b>
+        <p>Tengo varios intereses, pero los mas destacados son, ver o jugar futbol, mi equipo favorito es el FC Barcelonar, ademas de jugar videojugos ya sea en el celular, computador o alguna otra plataforma </p>
+    </div>
+</div>
+
+<div class="flex items-center space-x-10">
+    <div class="w-1/12" >
+        <img 
+            class="rounded-full"
+            src="<?php echo get_template_directory_uri()?>/assets/img/java.png" 
+            alt="imagen de perfil anthony">
+    </div>
+    <div class="w-6/12">
+        <b>Lenguaje de Programacion </b>
+        <p>En uno de los lenguajes que mas he trabajado es Java, aunque creo que no he usado todo el potencial de java, y que me falta conocer demasiado de este lenguaje de programacion, tambien he trabajado con visual basic y en realidad este lenguaje de programacion me agrada mucho.</p>
+    </div>
+</div>
+
+<div class="flex items-center space-x-10">
+    <div class="w-1/12" >
+        <img 
+            class="rounded-full"
+            src="<?php echo get_template_directory_uri()?>/assets/img/experiencia.jpg" 
+            alt="imagen de perfil anthony">
+    </div>
+    <div class="w-6/12">
+        <b>Experiencia Laboral </b>
+        <p>Actualmente no he podido adquirir experiencia laboral referente a la carrera que estudio, y eso ha sido muy frustrante ya que para poder conseguir un trabajo se necesita de mucha experiencia laboral, de igual manera no he realizado mis practicas pre-profesionales</p>
+    </div>
+</div>
+</main>
 <?php get_footer(); ?>
